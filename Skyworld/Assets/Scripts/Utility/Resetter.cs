58e36657fa243b922b135edd3c9cd4eba167c726
Ben Gardner <1bengardner@gmail.com>
2021-01-25 02:28:30 -0500

    Show a puff when player position is reset

--- Skyworld/Assets/Scripts/Utility/Resetter.cs
+++ Skyworld/Assets/Scripts/Utility/Resetter.cs
@@ -5,12 +5,14 @@ public class Resetter : MonoBehaviour {
     [SerializeField] Transform target;
     [SerializeField] Vector2 minExtents;
     [SerializeField] Vector2 maxExtents;
+    [SerializeField] GameObject respawnEffect;
 
 	// Update is called once per frame
 	void Update () {
 		if (target.localPosition.x < minExtents.x || target.localPosition.x > maxExtents.x || target.localPosition.y < minExtents.y || target.localPosition.y > maxExtents.y)
         {
             target.position = GameManager.Instance.currentZone.transform.position + Vector3.up;
+            Instantiate(respawnEffect, GameManager.Instance.currentZone.transform.position, Quaternion.identity);
         }
 	}
 }
